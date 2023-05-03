7f78f171ac8a644663efe00ddce45bcecf4ecd64
botirjon <botirjon.nasridinov@gmail.com>
2023-05-03 16:06:18 +0500

    bnappfoundation dep

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0"))
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
+        .package(url: "https://github.com/botirjon/BNAppFoundation.git", from: "1.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
